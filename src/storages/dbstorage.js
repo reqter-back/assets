71e5eb4b93225fd03f609356b29b8bd05060540f
fakhrad <fakhrad@openratio.com>
2019-10-14 11:17:04 +0330

    console log added

--- src/storages/dbstorage.js
+++ src/storages/dbstorage.js
@@ -1,15 +1,20 @@
-var GridFSStorage  = require('multer-gridfs-storage');
-var path = require('path');
-var fs = require('fs');
-var MongoClient = require('mongodb');
-var url = process.env.FileDB_URL || 'mongodb://fakhrad:logrezaee24359@ds231537.mlab.com:31537/files'
+var GridFSStorage = require("multer-gridfs-storage");
+var path = require("path");
+var fs = require("fs");
+var MongoClient = require("mongodb");
+var url =
+  process.env.FileDB_URL ||
+  "mongodb://fakhrad:logrezaee24359@ds231537.mlab.com:31537/files";
 var storage = undefined;
-storage = new GridFSStorage({ url: url, cache : true ,
-    file: (req, file) => {
-        var p = path.extname(file.originalname.toString());
-        
-        return file.fieldname + '-' + Date.now() + p;
-      }});
+storage = new GridFSStorage({
+  url: url,
+  cache: true,
+  file: (req, file) => {
+    console.log(file);
+    var p = path.extname(file.originalname.toString());
+
+    return file.fieldname + "-" + Date.now() + p;
+  }
+});
 
 module.exports = storage;
-
